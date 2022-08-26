1f839695ae2f5db80d777b8e87d9036d9c469acb
Vitalii G <merkant13@gmail.com>
2022-08-26 21:42:22 +0000

    Update Styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello World</h1>
+      <p>Something happens</p>
+      <h3>The more the Marrier</h3>
     </div>
   );
 }
